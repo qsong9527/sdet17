33d66ed67b31bdd59a3fb576f10f866f20100616
wohefakerwuwukai <2504592872@qq.com>
2020-12-11 15:28:44 +0800

    这是我在sdet17项目中的第二段代码////////

--- ChenJiayi/Dome/dome3.py
+++ ChenJiayi/Dome/dome3.py
@@ -1,10 +1,10 @@
-number = input("请输入您要查询的年份：")
+number = int(input("请输入您要查询的年份："))
 if number % 4 == 0 and number % 100 != 0:
-    print("{0}是润年")
+    print(f"{number}是润年")
 elif number % 100 == 0 and number % 400 == 0:
-    print("{0}是世纪润年")
+    print(f"{number}是世纪润年")
 else:
-    print("{0}不是润年")
+    print(f"{number}不是润年")
 
 
 
---
+++ fubo/demo ifelse.py
@@ -0,0 +1,8 @@
+a = int(input("请输入一个数字"))
+b = int(input("请输入一个数字"))
+if (a<b):
+    print(f"{a}<{b}")
+elif (a<b):
+    print(f"{a}>{b}")
+else:
+    print(f"{a}<{b}")
